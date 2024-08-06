21512e0cf074f3e798239dfa9b3b8adc96dc584b
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-06 17:33:38 +0300

    Add Delete  /states/state_id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,6 +9,8 @@ from models import storage
 
 @app_views.route('/states', methods=['GET'])
 def get_all_states():
+    """ get all states
+    """
     state_objs = storage.all(State)
     result = []
     for state in state_objs:
@@ -18,8 +20,21 @@ def get_all_states():
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
+    """ get state by id
+    """
     state_obj = storage.get(State, state_id)
     if not state_obj:
         abort(404)
     result = state_obj.to_dict()
     return jsonify(result), 200
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """ delete state by id
+    """
+    state_obj = storage.get(State, state_id)
+    if not state_obj:
+        abort(404)
+    storage.delete(state_obj)
+    return jsonify({}), 200
