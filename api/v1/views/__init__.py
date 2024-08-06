aa0078f737b3b58d3b64add9258df603652055b8
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-06 23:31:21 +0300

    Fix module documentations

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-initialize the models package
+    initialize the models package
 """
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
